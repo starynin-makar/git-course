c00c93af3a0ce27525e3640fc9e9a2793ff5cb50
Makar Starynin <54773277+qwerty1986ws@users.noreply.github.com>
2020-10-24 11:46:19 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,17 @@
+// console.log('ewrrewreewr')
+// const color = '12';
+// const obj = {a: 42}
+// obj.name = 'erek'
+// console.log(obj)
+
+// const sum = (a, b) => {
+//    return a*2
+// }
+// cons cube = a => a**3
+// console.log(sum(41, 2))
+// console.log(cube(2))
+// setTimeout (() => console.log('after 5 sec'), 5000)
+function log() {
+    console.log(this)
+}
+console.log(log)
